ccf72bf0f9613f66bcbbc3e9250d7fba41aebbfe
janieeuilee <leeeuiyoung4@gmail.com>
2018-11-02 14:12:51 +0900

    개인의 model과 admin 추가/ menu 사업자/개인 나누는 폼 추가

--- mysite/individual/admin.py
+++ mysite/individual/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from individual.models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('modify_date','description')
+    search_fields = ('title', 'content','description')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
